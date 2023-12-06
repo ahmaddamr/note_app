a9eebf25292ec9c02a0a20feee028748017c756d
ahmedamr <ahmed.eslam51a@gmail.com>
2023-12-07 01:35:58 +0200

    creating note item and adding poppins font

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/note_item.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -18,74 +19,23 @@ class NotesView extends StatelessWidget {
           ),
           actions: [
             Padding(
-              padding: const EdgeInsets.only(right: 10.0),
-              child: IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              padding: const EdgeInsets.only(right: 18.0, top: 6, bottom: 6),
+              child: Container(
+                height: 30,
+                width: 52,
+                // color: const Color.fromARGB(255, 146, 143, 143),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.white.withOpacity(0.1)),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                  iconSize: 25,
+                ),
+              ),
             )
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Container(
-              width: 385,
-              height: 200,
-              color: Color(0xffFFCD7A),
-              child: Row(
-                children: [
-                  const Padding(
-                    padding: const EdgeInsets.only(left: 27.0, top: 25),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Flutter tips',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 27,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 23,
-                        ),
-                        Text(
-                          'Build your career with \nTharwat Samy',
-                          style: TextStyle(
-                            color: const Color.fromARGB(255, 38, 36, 36),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 105.0, bottom: 82, top: 28),
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.delete),
-                          iconSize: 33,
-                        ),
-                      ),
-                      const Padding(
-                        padding: const EdgeInsets.only(left:43.0),
-                        child: Text('May 25,2023',style: TextStyle(
-                            color: const Color.fromARGB(255, 38, 36, 36),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      );
+        body: NoteItem());
   }
 }
---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Container(
+              // width: 385,
+              // height: 200,
+              padding:EdgeInsets.only(
+                top: 15,
+                bottom: 15
+              ) ,
+              color: Color(0xffFFCD7A),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: const EdgeInsets.only(left: 27.0, top: 5,bottom: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Flutter tips',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 23,
+                        ),
+                        Text(
+                          'Build your career with \nTharwat Samy',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 38, 36, 36),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 80.0, bottom: 70),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.black,
+                          ),
+                          iconSize: 33,
+                        ),
+                      ),
+                        Padding(
+                        padding: const EdgeInsets.only(left: 5.0,top:25),
+                        child: Text(
+                          'May 25,2023',
+                          style: TextStyle(
+                            color:  Colors.black.withOpacity(.5),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
