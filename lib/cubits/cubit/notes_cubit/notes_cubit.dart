9f75245fa03eee8e53f82248f089cdd3078e98a0
ahmedamr <ahmed.eslam51a@gmail.com>
2023-12-11 23:09:08 +0200

    create fetch notes method

--- lib/cubits/cubit/notes_cubit/notes_cubit.dart
+++ lib/cubits/cubit/notes_cubit/notes_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllanotes() async {
+    // emit(NotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>('Note_box');
+      emit(NotesSuccess(notesBox.values.toList()));
+    } catch (e) {
+      emit(NotesFailed((e.toString())));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/cubit/notes_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/custom_button.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
 import 'package:note_app/widgets/modal_bottom_sheet.dart';
@@ -9,53 +11,52 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () 
-          {
-            showModalBottomSheet(
-              isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              ),
-              context: context, builder: (context)
-            
-            {
-              return const  ModalBottomSheet() ;
-            });
-          },
-          child: const Icon(Icons.add),
-        ),
-        // backgroundColor: Color(0xff303030),
-        appBar: AppBar(
-          // backgroundColor: Color(0xff303030),
-          title: const Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: Text(
-              'Notes',
-              style: TextStyle(color: Colors.white, fontSize: 23),
-            ),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  context: context,
+                  builder: (context) {
+                    return const ModalBottomSheet();
+                  });
+            },
+            child: const Icon(Icons.add),
           ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 18.0, top: 6, bottom: 6),
-              child: Container(
-                height: 30,
-                width: 52,
-                // color: const Color.fromARGB(255, 146, 143, 143),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.white.withOpacity(0.1)),
-                child: IconButton(
-                  onPressed: () {},
-                  icon:const Icon(Icons.search),
-                  iconSize: 25,
-                ),
+          // backgroundColor: Color(0xff303030),
+          appBar: AppBar(
+            // backgroundColor: Color(0xff303030),
+            title: const Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text(
+                'Notes',
+                style: TextStyle(color: Colors.white, fontSize: 23),
               ),
-            )
-          ],
-        ),
-        body:const CustomListView());
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 18.0, top: 6, bottom: 6),
+                child: Container(
+                  height: 30,
+                  width: 52,
+                  // color: const Color.fromARGB(255, 146, 143, 143),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.white.withOpacity(0.1)),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                    iconSize: 25,
+                  ),
+                ),
+              )
+            ],
+          ),
+          body: const CustomListView()),
+    );
   }
 }
-
