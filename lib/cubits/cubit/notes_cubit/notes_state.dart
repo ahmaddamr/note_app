e5eee806e0781aaf1d7c15459ab7df754b451f49
ahmedamr <ahmed.eslam51a@gmail.com>
2023-12-11 22:37:02 +0200

    create notes state and add states

---
+++ lib/cubits/cubit/notes_cubit/notes_state.dart
@@ -0,0 +1,25 @@
+part of 'notes_cubit.dart';
+
+sealed class NotesState extends Equatable {
+  const NotesState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NotesInitial extends NotesState {}
+final class NotesLoading extends NotesState {}
+final class NotesSuccess extends NotesState {
+    final List<NoteModel> notes;
+
+  NotesSuccess(this.notes);
+}
+
+final class NotesFailed extends NotesState {
+  final String errMessage ;
+
+  NotesFailed(this.errMessage);
+}
+
+
+
